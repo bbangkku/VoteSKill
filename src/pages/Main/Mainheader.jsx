9812bf56e66754060f8430fe3d02c57c54f3d837
JungInLee0130 <dlwjddls0130@naver.com>
2023-07-19 10:55:24 +0900

    :lipstick: Style: 메인화면 UI
    메인화면 UI 개발

---
+++ src/pages/Main/Mainheader.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+function Mainheader(){
+
+    const Main = styled.div`
+            
+    `;
+
+    const name = 'react';
+    const message = `hello ${name}`;
+
+    console.log(message);
+
+    return(
+        <div>
+            hello
+        </div>
+    );
+}
+
+export default Mainheader;
