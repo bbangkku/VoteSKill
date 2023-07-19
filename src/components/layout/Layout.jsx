a0e6d955f61dd6db12110e633568ef99cae5e38c
JungInLee0130 <dlwjddls0130@naver.com>
2023-07-19 17:25:31 +0900

    :lipstick: Style: merge conflict 관련 코드 제거

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,26 +1,14 @@
-import * as S from 'components/layout/Layout.Style';
+import * as S from "components/layout/Layout.Style";
 
-<<<<<<< HEAD
-function Layout({isMain}){
+function Layout(){
     return (
         <div>
             <S.Background>
                 {/* {isMain ? <S.MainLogo></S.MainLogo> : null} */}
-                {isMain && <S.MainLogo src={process.env.PUBLIC_URL + '/image/voteskill_logo.png'} alt="voteskill" />}
             </S.Background>
         </div>
     );
-=======
-function Layout({ isMain }) {
-  return (
-    <div>
-      <S.Background>
-        {/* {isMain ? <S.MainLogo></S.MainLogo> : null} */}
-        {isMain && <S.MainLogo />}
-      </S.Background>
-    </div>
-  );
->>>>>>> 08db3ae25106f17a4200d353d2076d641ee298c5
 }
+
 
 export default Layout;
