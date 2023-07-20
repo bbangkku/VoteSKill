1b6ec7161c7f41c4559eafeab5c110cc5f09087c
Seokjun7074 <sukjune96@gmail.com>
2023-07-20 15:54:57 +0900

    :sparkles: Feat : Header 생성

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,8 @@
 import Main from 'pages/Main/Main';
 import SignIn from 'pages/SignIn/SignIn';
+import TestPage from 'pages/TestPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function Router() {
   return (
     <BrowserRouter>
@@ -10,6 +12,8 @@ function Router() {
         <Route path="/lobby" />
         <Route path="/room/:roomId" />
         <Route path="/game/:gameId" />
+
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,13 @@
+import Header from 'components/header/Header';
+import Layout from 'components/layout/Layout';
+
+function TestPage() {
+  return (
+    <>
+      <Layout></Layout>
+      <Header />
+    </>
+  );
+}
+
+export default TestPage;
