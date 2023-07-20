a1cb67ffd2a7372e4d045bc2c9658e78cbbcf263
JungInLee0130 <dlwjddls0130@naver.com>
2023-07-20 09:46:27 +0900

    :lipstick: Style: 회원가입 UI fix

--- src/components/layout/Layout.Style.js
+++ src/components/layout/Layout.Style.js
@@ -7,14 +7,13 @@ export const Background = styled.div`
   background-repeat: no-repeat;
   background-position: 50% 100%;
   background-size: 100%;
+  position: fixed;
 `;
 
-export const MainLogo = styled.div`
-  background-image: url(${process.env.PUBLIC_URL + '/image/voteskill_logo.svg'});
-  background-repeat: no-repeat;
-  background-size: 79% 100%;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  left: 10%;
+export const MainLogo = styled.img`
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
