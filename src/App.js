084a53424687cf1585d90e2642a6811233a251c8
Seokjun7074 <sukjune96@gmail.com>
2023-07-21 14:01:29 +0900

    :sparkles: Feat : 쿠키 사용 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from 'Router';
 import { RecoilRoot } from 'recoil';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <Router />
-        </RecoilRoot>
-      </QueryClientProvider>
+      <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <Router />
+          </RecoilRoot>
+        </QueryClientProvider>
+      </CookiesProvider>
     </div>
   );
 }
