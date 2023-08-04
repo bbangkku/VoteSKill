edf94ad2226a8eee9d3f7cca41ecc10d69f84e76
Seokjun7074 <sukjune96@gmail.com>
2023-08-04 16:36:38 +0900

    :sparkles: Feat : 채팅 기능 구현

--- src/pages/WaitingRoom/Chatting/Chatting.jsx
+++ src/pages/WaitingRoom/Chatting/Chatting.jsx
@@ -1,38 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as S from 'pages/WaitingRoom/Chatting/Chatting.style';
-function Chatting() {
-  const dummyMessages = [
-    {
-      nickname: 'test1',
-      message:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, deserunt iusto! Veritatis expedita velit illo cumque officia neque inventore totam corrupti molestiae laborum beatae, et veniam quam error voluptates a!',
-    },
-    {
-      nickname: 'test2',
-      message: 'message2',
-    },
-    {
-      nickname: 'test3',
-      message: 'message3',
-    },
-    {
-      nickname: 'test4',
-      message: 'message4',
-    },
-  ];
+function Chatting({ messageList, sendMessage }) {
+  const [inputMessage, setInputMessage] = useState({ message: '', nickname: 'testnickname' });
+  const messageEndRef = useRef();
+
+  useEffect(() => {
+    messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
+  }, [messageList]);
+
+  const handleInputMessage = (e) => {
+    setInputMessage({ ...inputMessage, message: e.target.value });
+  };
+
+  const sendChatting = () => {
+    sendMessage(inputMessage);
+    setInputMessage({ ...inputMessage, message: '' });
+  };
+
+  const handleOnKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      sendChatting(); // Enter 입력이 되면 클릭 이벤트 실행
+    }
+  };
 
   return (
     <S.Chat>
       <S.ChattingContainer>
-        {dummyMessages.map((msg) => (
-          <S.ChattingDivider key={msg.nickname} myMessage={msg.nickname == 'test3'}>
-            <S.MessageBox myMessage={msg.nickname == 'test3'}>{msg.message}</S.MessageBox>
+        {messageList.map((msg, idx) => (
+          <S.ChattingDivider key={idx} $mymessage={msg.nickname == 'test3'}>
+            <S.MessageBox $mymessage={msg.nickname == 'test3'}>{msg.message}</S.MessageBox>
           </S.ChattingDivider>
         ))}
+        <div ref={messageEndRef}></div>
       </S.ChattingContainer>
       <S.BottomBar>
-        <S.InputBar placeholder="메세지를 입력하세요" />
-        <S.SendButton>입력</S.SendButton>
+        <S.InputBar
+          placeholder="메세지를 입력하세요"
+          onChange={handleInputMessage}
+          value={inputMessage.message}
+          onKeyDown={handleOnKeyPress}
+        />
+        <S.SendButton onClick={sendChatting}>입력</S.SendButton>
       </S.BottomBar>
     </S.Chat>
   );
--- src/pages/WaitingRoom/Chatting/Chatting.style.js
+++ src/pages/WaitingRoom/Chatting/Chatting.style.js
@@ -2,23 +2,26 @@ import { styled } from 'styled-components';
 
 export const Chat = styled.div`
   width: 50%;
+  gap: 20px;
   display: flex;
   flex-direction: column;
-  padding: 10px;
-  margin-right: 200px;
-  gap: 20px;
+  justify-content: space-between;
 `;
 
 export const ChattingContainer = styled.div`
-  height: 100%;
+  height: 470px;
+  padding: 15px;
   border-radius: ${({ theme }) => theme.borderRadius.s};
-  padding: 10px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: ${({ theme }) => theme.color.lightgray};
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 export const ChattingDivider = styled.div`
   width: 100%;
   display: flex;
-  justify-content: ${({ myMessage }) => (myMessage ? 'end' : 'start')};
+  justify-content: ${({ $mymessage }) => ($mymessage ? 'end' : 'start')};
 `;
 
 export const MessageBox = styled.div`
@@ -26,9 +29,9 @@ export const MessageBox = styled.div`
   max-width: 40%;
   padding: 10px;
   overflow-wrap: break-word;
-  text-align: ${({ myMessage }) => (myMessage ? 'end' : 'start')};
-  background-color: ${({ theme, myMessage }) => (myMessage ? theme.color.gray : theme.color.lightgray)};
-  color: ${({ theme, myMessage }) => (myMessage ? theme.color.lightgray : theme.color.gray)};
+  text-align: ${({ $mymessage }) => ($mymessage ? 'end' : 'start')};
+  background-color: ${({ theme, $mymessage }) => ($mymessage ? theme.color.gray : theme.color.brown)};
+  color: ${({ theme, $mymessage }) => ($mymessage ? theme.color.lightgray : theme.color.lightgray)};
   border-radius: ${({ theme }) => theme.borderRadius.s};
   box-shadow: 1px 1px 2px black;
 `;
@@ -52,46 +55,3 @@ export const SendButton = styled.button`
   background-color: ${({ theme }) => theme.color.lightgray};
   border-radius: 10px;
 `;
-
-// export const StartButton = styled.button`
-//   background: #1aab8a;
-//   color: #fff;
-//   border: none;
-//   position: relative;
-//   height: 60px;
-//   font-size: 1.6em;
-//   padding: 0 2em;
-//   cursor: pointer;
-//   transition: 800ms ease all;
-//   outline: none;
-
-//   &:hover {
-//     background: #fff;
-//     color: #1aab8a;
-//   }
-
-//   &:before,
-//   &:after {
-//     content: '';
-//     position: absolute;
-//     top: 0;
-//     right: 0;
-//     height: 2px;
-//     width: 0;
-//     background: #1aab8a;
-//     transition: 400ms ease all;
-//   }
-
-//   &:after {
-//     right: inherit;
-//     top: inherit;
-//     left: 0;
-//     bottom: 0;
-//   }
-
-//   &:hover:before,
-//   &:hover:after {
-//     width: 100%;
-//     transition: 800ms ease all;
-//   }
-// `;
