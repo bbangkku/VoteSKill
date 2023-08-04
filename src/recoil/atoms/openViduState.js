4113fe5a9a6c1384f83500f0322c3852dce798bb
Seokjun7074 <sukjune96@gmail.com>
2023-08-04 11:16:52 +0900

    :recycle: Refactor : publisher 비디오세팅 변수 분리

--- src/recoil/atoms/openViduState.js
+++ src/recoil/atoms/openViduState.js
@@ -9,3 +9,17 @@ export const userNameState = atom({
   key: 'userNameState',
   default: '',
 });
+
+export const publisherState = atom({
+  key: 'publisherState',
+  default: {
+    audioSource: undefined,
+    videoSource: undefined,
+    publishAudio: true,
+    publishVideo: true,
+    resolution: '200x200',
+    frameRate: 60,
+    insertMode: 'APPEND',
+    mirror: true,
+  },
+});
