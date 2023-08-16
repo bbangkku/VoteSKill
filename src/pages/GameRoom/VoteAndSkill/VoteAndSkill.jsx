bdb82ad9a02b73500863ce0e94c1d7875985b4ec
JungInLee0130 <dlwjddls0130@naver.com>
2023-08-16 12:36:31 +0900

    :lipstick: Style: 직업 능력 아이콘 추가

--- src/pages/GameRoom/VoteAndSkill/VoteAndSkill.jsx
+++ src/pages/GameRoom/VoteAndSkill/VoteAndSkill.jsx
@@ -13,14 +13,12 @@ function VoteAndSkill(props) {
           onClick={() => resetVote(props.streamManager)}
         />
       )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'MAFIA' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_mafia.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
+      {props.isSkillTime && checkVote(props.streamManager) && props.myRole === 'MAFIA' && (
+        <SkillImage
+          src={process.env.PUBLIC_URL + '/image/game/skill_mafia.svg'}
+          onClick={() => resetVote(props.streamManager)}
+        />
+      )}
       {props.isSkillTime &&
         checkVote(props.streamManager) &&
         // 직업에따라 능력사용
@@ -30,46 +28,30 @@ function VoteAndSkill(props) {
             onClick={() => resetVote(props.streamManager)}
           />
         )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'DOCTOR' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_doctor.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'GANG' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_gang.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'REPORTER' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_reporter.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'POLITICIAN' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_politician.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
-      {props.isSkillTime &&
-        checkVote(props.streamManager) &&
-        props.myRole === 'POLITICIAN' && (
-          <SkillImage
-            src={process.env.PUBLIC_URL + '/image/game/skill_politician.svg'}
-            onClick={() => resetVote(props.streamManager)}
-          />
-        )}
+      {props.isSkillTime && checkVote(props.streamManager) && props.myRole === 'DOCTOR' && (
+        <SkillImage
+          src={process.env.PUBLIC_URL + '/image/game/skill_doctor.svg'}
+          onClick={() => resetVote(props.streamManager)}
+        />
+      )}
+      {props.isSkillTime && checkVote(props.streamManager) && props.myRole === 'GANG' && (
+        <SkillImage
+          src={process.env.PUBLIC_URL + '/image/game/skill_gang.svg'}
+          onClick={() => resetVote(props.streamManager)}
+        />
+      )}
+      {props.isSkillTime && checkVote(props.streamManager) && props.myRole === 'REPORTER' && (
+        <SkillImage
+          src={process.env.PUBLIC_URL + '/image/game/skill_reporter.svg'}
+          onClick={() => resetVote(props.streamManager)}
+        />
+      )}
+      {props.isSkillTime && checkVote(props.streamManager) && props.myRole === 'PRIEST' && (
+        <SkillImage
+          src={process.env.PUBLIC_URL + '/image/game/skill_priest.svg'}
+          onClick={() => resetVote(props.streamManager)}
+        />
+      )}
     </>
   );
 }
