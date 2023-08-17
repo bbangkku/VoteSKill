343117725a1c1c2163dc728756007d3145c254d6
Seokjun7074 <sukjune96@gmail.com>
2023-08-17 12:57:07 +0900

    :lipstick: Style : 타이머 색상 변경

--- src/components/timer/Timer.style.js
+++ src/components/timer/Timer.style.js
@@ -24,11 +24,11 @@ export const IconBox = styled.div`
   width: 30px;
   height: 30px;
   animation: ${rotate} 1s linear infinite;
-  color: ${({ $sec, theme }) => ($sec <= 10 ? theme.color.red : theme.color.gray)};
+  color: ${({ $sec, theme }) => ($sec <= 10 ? theme.color.red : theme.color.lightgray)};
 `;
 
 export const TimeSpan = styled.span`
   font-size: 25px;
   font-weight: bold;
-  color: ${({ $sec, theme }) => ($sec <= 10 ? theme.color.red : theme.color.gray)};
+  color: ${({ $sec, theme }) => ($sec <= 10 ? theme.color.red : theme.color.lightgray)};
 `;
